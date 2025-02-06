174d842a4707884bb637875c6d6ba0cc88456b88
Amir Mohammad Mohammadi <95334969+Qarebaq@users.noreply.github.com>
2025-02-06 14:19:34 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function toggleBox() {
+    var box = document.getElementById('box');
+    if (box.style.display === 'none' || box.style.display === '') {
+        box.style.display = 'block';
+    } else {
+        box.style.display = 'none';
+    }
+}
+
+
+
+
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+// Show or hide the button based on scroll position
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+// Scroll to the top when the button is clicked
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
